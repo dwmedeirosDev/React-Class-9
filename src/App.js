74505ecab8504fae8d9158538302861bac407c4e
dwmedeiros94 <davidmedeiros94@hotmail.com>
2022-07-25 20:07:16 -0300

    OK

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+// styled-component
+// Instalar o style-component no terminal => npm install styled-component
+// Importar o style-component => import styled from 'styled-components' 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import styled from 'styled-components' 
+
+export const Calculadora = styled.div`
+    background-color: blueviolet;
+    max-width: 400px;
+    width: 100%;
+    margin: 0 auto;
+    height: 380px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    border-radius: 5px;
+
+  input{
+    width: 40%;
+    padding: 0.8rem;
+    border-radius: 5px;
+    border: none;
+    outline: none;
+  }
+
+  div{
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+  }
+
+  button{
+    background-color: white;
+    border: none;
+    width: 40px;
+    height: 30px;
+    cursor: pointer;
+  }
+
+  h2{
+    width: 40%;
+    height: 50px;
+    background-color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
+export default class App extends Component{
+    state = {
+        n1: '',
+        n2: '',
+        resultado: ''
+      }
+    
+      divisao = () => {
+        this.setState({
+          resultado: this.state.n1 / this.state.n2
+        })
+      }
+
+// Buscando valor do Input
+
+    handleChange1 = (event) => {
+        this.setState({
+        n1:event.target.value
+        })
+    }
+
+    handleChange2 = (event) => {
+        this.setState({
+        n2: event.target.value
+        })
+    }
+
+    render(){
+        return(
+            <Calculadora>
+                <h1>Calculadora</h1>
+                <input type="number" onChange={this.handleChange1} />
+                <input type="number" onChange={this.handleChange2}/>
+                <div>
+                    <button>+</button>
+                    <button>-</button>
+                    <button onClick={this.divisao}>/</button>
+                    <button>*</button>
+                    <button>c</button>
+                </div>
+                <h2>{this.state.resultado}</h2>
+            </Calculadora>
+        )
+    }
 }
-
-export default App;
